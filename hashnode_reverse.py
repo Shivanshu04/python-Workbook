d6b85bd1ee1d98fbc345c47947f8af515e666859
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-03 14:06:07 +0530

    nw

--- hashnode_reverse.py
+++ hashnode_reverse.py
@@ -4,3 +4,11 @@ def reverse_string (str):
     return str[::-1]
 my_txt = reverse_string(" i am going to mumbai")
 print(my_txt)
+
+#method 2 using reverse function
+def reverse_string(str):
+    return ''.join(reversed(str))
+my_txt =reverse_string("how")
+print(my_txt)
+
+#method
