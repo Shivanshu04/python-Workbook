825d9b3b4265299dad48f667c5712882de29de27
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-03 13:56:59 +0530

    first prrogram

---
+++ Reverse.py
@@ -0,0 +1,9 @@
+#reverse string  use  simple method
+txt = "Shivanshu"[::-1]
+print(txt)
+
+# by using the function method
+def my_function(x):
+    return x[::-1]
+var = my_function("hello everyone")
+print(var)
