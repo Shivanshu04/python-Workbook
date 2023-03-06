21df371d72981c62b85da94151a9e53ed5eccb47
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-06 21:23:42 +0530

    new method

--- hashnode_reverse.py
+++ hashnode_reverse.py
@@ -17,4 +17,23 @@ def reverse_string(str):
     for i in range(len(str)-1, -1, -1):
         new_str += str[i]
     return new_str
+#method 4:- by using recursion
+def reverse_string(str):
+    if len(str) == 0:
+        return str
+    else:
+        return reverse_string(str[1:]) + str[0]
+#mrthod 5:- by using while loop
+def reverse_string(str):
+    new_str = ''
+    i = len(str)-1
+    while i >= 0:
+        new_str += str[i]
+        i -= 1
+    return new_str
+#method 6 :- using list comprehension
 
+def reverse_string(str):
+    return ''.join([str[i] for i in range(len(str)-1, -1, -1)])
+
+# the slice notation method (Method 1) and the reverse function method (Method 2) are likely to be the most efficient ways to reverse a string in Python.
