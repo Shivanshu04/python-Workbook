3ab82ecd7b645c365cb2a45c0afc2467343cdfb7
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-16 18:03:54 +0530

    swap

---
+++ swap.py
@@ -0,0 +1,15 @@
+#sawppin program without using Third variable
+a=int(input())
+b=int(input())
+print("Value of a is :" ,a)
+print("Value of b is :",b)
+print("After swapping ")
+#a,b=b,a  for without using third variable
+temp=a
+a=b
+b=temp
+
+print("Value of a is :" ,a)
+print("Value of b is :",b)
+
+
