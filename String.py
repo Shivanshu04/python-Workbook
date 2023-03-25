14b681014adbf2ded3a971155e45bea825b67244
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-25 14:43:19 +0530

    new ,method

--- String.py
+++ String.py
@@ -4,3 +4,16 @@ S1 ='Shivanshu' # use single quote
 S2 = "Shivu" # use double quote
 print(type(S1))
 print(type(S2))
+
+# use triple quote to create docstring / multiline strings
+a = '''hello i am shivanshu gupta
+i build most sucessful statrup in india
+and provide lots of jobs in india
+and featured in forbes under 30'''
+print(a) #multiline String
+
+#for new line we use \n
+a = "Shivanshu\nGupta"
+print(a)
+
+
