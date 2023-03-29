24950b4c9f3d41266d4b138056f033a303e6c57e
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-29 17:07:45 +0530

    new

--- project/lisr.py
+++ project/lisr.py
@@ -3,3 +3,10 @@ print(a[5])
 print(a[2:5])
 print(a[:7])
 print(a[5:])
+
+#manupulation of list
+data = ["aman",11,"shivu",23,"sonu",56,"mukesh",32]
+print(data)
+#update sonu age 23
+data[5]=23
+print(data)
