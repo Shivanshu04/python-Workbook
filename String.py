c71928e0d1ac5f5e2f52515d3f084c158e8632a4
Shivanshu04 <shivu2jaun@gmail.com>
2023-04-08 16:00:57 +0530

    new

--- String.py
+++ String.py
@@ -5,28 +5,28 @@ S2 = "Shivu" # use double quote
 print(type(S1))
 print(type(S2))
 
-# use triple quote to create docstring / multiline strings
-a = '''hello i am shivanshu gupta
-i build most sucessful statrup in india
-and provide lots of jobs in india
-and featured in forbes under 30'''
-print(a) #multiline String
+# # use triple quote to create docstring / multiline strings
+# a = '''hello i am shivanshu gupta
+# i build most sucessful statrup in india
+# and provide lots of jobs in india
+# and featured in forbes under 30'''
+# print(a) #multiline String
 
-#for new line we use \n
-a = "Shivanshu\nGupta"
-print(a)
+# #for new line we use \n
+# a = "Shivanshu\nGupta"
+# print(a)
 
-#acessing the character in string
-s= 'hello'
-print(s[0])
-print(s[3])
-print(s[4])
- # negative  indexingp
-print("anothe method")
-s= 'hello'
-print(s[-2])
-print(s[-3])
-print(s[-4])
+# #acessing the character in string
+# s= 'hello'
+# print(s[0])
+# print(s[3])
+# print(s[4])
+#  # negative  indexingp
+# print("anothe method")
+# s= 'hello'
+# print(s[-2])
+# print(s[-3])
+# print(s[-4])
 
 # string Concatenation 
 #We can perform string concatenation using the following ways:
