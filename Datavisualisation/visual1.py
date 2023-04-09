5a02c7de10939f0c1c8da91f94021cfb8d672a07
Shivanshu04 <shivu2jaun@gmail.com>
2023-04-09 23:49:15 +0530

    # Print the last item from year and pop print(year[-1]) print(pop[-1])
    # Import matplotlib.pyplot as plt
    import matplotlib.pyplot as plt
    
    # Make a line plot: year on the x-axis, pop on the y-axis
    plt.plot(year, pop)
    
    # Display the plot with plt.show()
    plt.show()

---
+++ Datavisualisation/visual1.py
@@ -0,0 +1,25 @@
+# Print the last item from year and pop
+year =[1999,2006,2011,2019,2023]
+pop = [40,50,60,70,90]
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year, pop)
+
+# Display the plot with plt.show()
+plt.show()
+
+# Print the last item from year and pop
+year =[1999,2006,2011,2019,2023]
+pop = [40,50,60,70,90]
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.scatter(year, pop)
+
+# Display the plot with plt.show()
+plt.show()
